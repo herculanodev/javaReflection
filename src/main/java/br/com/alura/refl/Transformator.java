101dfe1f28d013cb34c8a880ec7ab63b42141842
Daniel Herculano <dherculano93@gmail.com>
2025-04-02 17:52:15 -0300

    subindo reflection

---
+++ src/main/java/br/com/alura/refl/Transformator.java
@@ -0,0 +1,43 @@
+package br.com.alura.refl;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Transformator {
+
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Class<?> source= input.getClass();
+        Class<?> target= Class.forName(source.getClass() + "DTO");
+
+        O targetCLass = (O) target.getDeclaredConstructor().newInstance();
+
+        Field[] sourceFields = source.getDeclaredFields();
+        Field[] targetFields =  target.getDeclaredFields();
+
+        Arrays.stream(sourceFields).forEach(sourceField ->
+                Arrays.stream(targetFields).forEach(targetField ->{
+                        validate(sourceField, targetField);
+                    try {
+                        targetField.set(targetCLass, sourceField.get(input));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }));
+
+
+        return targetCLass;
+
+
+
+    }
+    private void validate(Field sourceField, Field targetField) {
+        if (sourceField.getName().equals(targetField.getName()) && sourceField.getType().equals(targetField.getType())) {
+            sourceField.setAccessible(true);
+            targetField.setAccessible(true);
+        } else {
+            System.out.println("Os campos são diferentes");
+        }
+    }
+
+}
